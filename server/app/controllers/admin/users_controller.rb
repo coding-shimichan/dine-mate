5f16d9175f86c3fc0c42a8b4ca307f1f08a092f4
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-03-07 15:47:53 +0900

    Test: admin/user

--- server/app/controllers/admin/users_controller.rb
+++ server/app/controllers/admin/users_controller.rb
@@ -4,6 +4,15 @@ class Admin::UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html { render :index, status: :ok }
+      format.json { render json: @users, status: :ok }
+    end
+  end
+
+  def new
+    @user = User.new
   end
 
   def show
@@ -14,19 +23,42 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: "User was successfully created.", status: :created }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to admin_users_path, notice: "ユーザー情報を更新しました。"
-    else
-      render :edit
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admin_user_path(@user), notice: "User was successfully updated.", status: :ok }
+        format.json { render json: @user, status: :ok }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path, notice: "ユーザーを削除しました。"
+
+    respond_to do |format|
+      format.html { redirect_to admin_users_path, notice: "ユーザーを削除しました。", status: :see_other }
+      format.json { head :no_content }
+    end 
   end
 
   private
@@ -36,6 +68,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :admin)
+    params.require(:user).permit(:email, :password, :password_confirmation, :admin)
   end
 end
--- server/spec/requests/admin/users_spec.rb
+++ server/spec/requests/admin/users_spec.rb
@@ -1,65 +1,120 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe "User management", type: :request do
-#   describe "User CRUD for admins" do
-#     let!(:admin_user) { FactoryBot.create(:admin_user) }
-#     let!(:first_user) { FactoryBot.create(:first_user) }
-#     let!(:restaurant) { FactoryBot.create(:restaurant) }
-#     let!(:user) { FactoryBot.create(:user, {user_id: first_user.id, restaurant_id: restaurant.id}) }
+RSpec.describe "User management", type: :request do
+  describe "User CRUD for admins" do
+    let!(:admin_user) { FactoryBot.create(:admin_user) }
+    let(:first_user) { FactoryBot.create(:first_user) }
 
-#     context "Logged in as admin_user, requests HTTP" do
-#       before do
-#         sign_in admin_user
-#       end
+    context "Logged in as admin_user, requests HTTP" do
+      before do
+        sign_in admin_user
+      end
 
-#       it "GET admin_users" do
-#         get "/admin/users"
-#         expect(response).to have_http_status(:success)
-#       end
+      it "Can GET /admin/users" do
+        get "/admin/users"
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Can POST /admin/users" do
+        post "/admin/users", :params => { user: { email: "second.user@aaa.com", password: "password", password_confirmation: "password", admin: false } }
+        expect(response).to have_http_status(:created)
+      end
       
-#       it "GET admin_user" do
-#         get "/admin/users/#{user.id}"
-#         expect(response).to have_http_status(:success)
-#       end
+      it "Can GET /admin/users/new" do
+        get "/admin/users/new"
+        expect(response).to have_http_status(:ok)
+      end
 
-#       it "DELETE admin_user" do
-#         delete "/admin/users/#{user.id}"
-#         expect(response).to have_http_status(:see_other)
-#       end
-#     end
+      it "Can GET /admin/users/:id/edit for an admin user" do
+        get "/admin/users/#{admin_user.id}/edit"
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Can GET /admin/users/:id/edit for a normal user" do
+        get "/admin/users/#{first_user.id}/edit"
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Can PUT /admin/users/:id for an admin user" do
+        put "/admin/users/#{admin_user.id}", :params => { user: {email: "admin.user2@aaa.com" } }
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "Can PUT /admin/users/:id for a normal user" do
+        put "/admin/users/#{first_user.id}", :params => { user: {email: "first.user2@aaa.com" } }
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "DELETE /admin/users/:id for an admin user" do
+        delete "/admin/users/#{admin_user.id}"
+        expect(response).to have_http_status(:see_other)
+      end
+    end
     
-#     context "Logged in as admin_user, requests JSON" do
-#       before do
-#         sign_in admin_user
-#       end
+    context "Logged in as admin_user, requests JSON" do
+      before do
+        sign_in admin_user
+      end
 
-#       headers = { "ACCEPT" => "application/json" }
+      headers = { "ACCEPT" => "application/json" }
 
-#       it "GET admin_users" do
-#         get "/admin/users", :headers => headers
-#         json_response = JSON.parse(response.body)
+      it "Can GET /admin/users" do
+        get "/admin/users", :headers => headers
+        expect(response).to have_http_status(:ok)
+      end
 
-#         expect(response).to have_http_status(:ok)
-#         expect(json_response.length).to eq(User.all.length)
-#       end
+      it "Can POST /admin/users" do
+        post "/admin/users", :params => { user: { email: "second.user@aaa.com", password: "password", password_confirmation: "password", admin: false } }, :headers => headers
+        expect(response).to have_http_status(:created)
+      end
       
-#       it "GET admin_user" do
-#         get "/admin/users/#{user.id}", :headers => headers
-#         json_response = JSON.parse(response.body)
+      it "Can PUT /admin/users/:id for an admin user" do
+        put "/admin/users/#{admin_user.id}", :params => { user: { email: "updated.admin@aaa.com" } }, :headers => headers
+        expect(response).to have_http_status(:ok)
+      end
 
-#         expect(response).to have_http_status(:ok)
-#         expect(json_response["title"]).to eq(user.title)
-#         expect(json_response["content"]).to eq(user.content)
-#         expect(json_response["user_id"]).to eq(user.user_id)
-#         expect(json_response["restaurant_id"]).to eq(user.restaurant_id)
-#       end
+      it "Can PUT /admin/users/:id for a normal user" do
+        put "/admin/users/#{first_user.id}", :params => {user: { email: "updated.first@aaa.com" }}, :headers => headers
+        expect(response).to have_http_status(:ok)
+      end
 
-#       it "DELETE admin_user" do
-#         delete "/admin/users/#{user.id}", :headers => headers
-#         # json_response not available as response is empty
+      it "DELETE /admin/users/:id for an admin user" do
+        delete "/admin/users/#{admin_user.id}", :headers => headers
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+    
+    context "Logged in as normal user, requests JSON" do
+      before do
+        sign_in first_user
+      end
 
-#         expect(response).to have_http_status(:no_content)
-#       end
-#     end
-#   end
-# end
+      headers = { "ACCEPT" => "application/json" }
+
+      it "Cannot GET /admin/users" do
+        get "/admin/users", :headers => headers
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it "Can POST /admin/users" do
+        post "/admin/users", :params => { user: { email: "second.user@aaa.com", password: "password", password_confirmation: "password", admin: false } }, :headers => headers
+        expect(response).to have_http_status(:forbidden)
+      end
+      
+      it "Can PUT /admin/users/:id for an admin user" do
+        put "/admin/users/#{admin_user.id}", :params => { user: {email: "admin.user2@aaa.com" } }, :headers => headers
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it "Can PUT /admin/users/:id for a normal user" do
+        put "/admin/users/#{first_user.id}", :params => { user: {email: "first.user2@aaa.com" } }, :headers => headers
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it "DELETE /admin/users/:id for an normal user" do
+        delete "/admin/users/#{admin_user.id}", :headers => headers
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+end
