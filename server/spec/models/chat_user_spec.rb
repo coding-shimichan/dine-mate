0df8f619766524c8bc587d211cbab7325f966f4e
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-03-11 11:56:02 +0900

    Change: Chat must have exactly two participants

--- server/spec/models/chat_user_spec.rb
+++ server/spec/models/chat_user_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe ChatUser, type: :model do
     it { should belong_to(:chat) }
     it { should belong_to(:user) }
   end
+
+  describe "Chat is restricted to have only two participants" do
+    let!(:first_user) { FactoryBot.create(:first_user) }
+    let!(:second_user) { FactoryBot.create(:second_user) }
+    let!(:third_user) { FactoryBot.create(:third_user) }
+    let!(:chat) { FactoryBot.create(:chat) }
+
+    it "fails to create third participatns" do
+      ChatUser.create!(chat_id: chat.id, user_id: first_user.id)
+      ChatUser.create!(chat_id: chat.id, user_id: second_user.id)
+
+      third_chat_user = ChatUser.new(chat_id: chat.id, user_id: third_user.id)
+      expect(third_chat_user).not_to be_valid
+      expect(third_chat_user.errors[:chat]).to include("can only have two participants")
+    end
+  end
 end
