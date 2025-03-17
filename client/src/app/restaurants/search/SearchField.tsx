0d82af7d6ca2748411a00ddce311b607a90ac8ac
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-03-17 11:48:58 +0900

    Add: restaurants/search page

---
+++ client/src/app/restaurants/search/SearchField.tsx
@@ -0,0 +1,12 @@
+export default function SearchField() {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search restaurants..."
+        id="search-field"
+      ></input>
+      <button type="submit">Submit</button>
+    </>
+  );
+}
