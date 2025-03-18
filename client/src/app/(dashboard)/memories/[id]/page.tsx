bc015da2b78d3dfb6ae35cdbceadaab2d435a2f1
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-03-18 11:10:00 +0900

    Feat: populate data for memory page using backend API

--- client/src/app/(dashboard)/memories/[id]/page.tsx
+++ client/src/app/(dashboard)/memories/[id]/page.tsx
@@ -1,20 +1,25 @@
+import Date from "@/app/components/date";
 import Memory from "@/app/types/Memory";
 
-export default function MemoryPage() {
-  const memory: Memory = {
-    id: "1",
-    title: "Memory 1",
-    content: "Memory 1 content",
-    mainImageSrc: "/photo-materials/photo2.jpg",
-    restaurant_id: "1",
-  };
+export default async function MemoryPage() {
+  const API_URL = process.env.RAILS_API_URL;
+  const data = await fetch(`${API_URL}/users/1/memories/4`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const memory: Memory = await data.json();
 
   return (
     <>
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         {memory.title}
       </h1>
-      <div>
+      <div className="mb-4 text-gray-900 dark:text-white">
+        <Date dateString={memory.created_at} />
+      </div>
+      <div className="mb-4 text-gray-900 dark:text-white">
         <p>{memory.content}</p>
       </div>
     </>
