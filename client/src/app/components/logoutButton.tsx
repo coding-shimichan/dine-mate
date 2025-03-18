49221ae831130a237e7abc0edd39958e7707e2a8
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-03-18 21:46:22 +0900

    Chore: cursor pointer on LogoutButton

--- client/src/app/components/logoutButton.tsx
+++ client/src/app/components/logoutButton.tsx
@@ -38,7 +38,7 @@ export default function LogoutButton() {
   return (
     <button
       type="button"
-      className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+      className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 cursor-pointer"
       onClick={handleLogout}
     >
       Logout
