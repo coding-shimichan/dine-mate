712eb50cb1011b2e6fcddfbfc2b03e4ea15bb5fa
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-03-21 16:06:29 +0900

    Refactor: remove unused format

--- server/app/controllers/api/memories_controller.rb
+++ server/app/controllers/api/memories_controller.rb
@@ -1,39 +1,36 @@
 class Api::MemoriesController < ApplicationController
-  before_action :set_memory, only: %i[ show update destroy ]
   before_action :authenticate_devise_api_token!
+  before_action :set_memory, only: %i[ show update destroy ]
 
-  # GET /memories
+  # GET /api/memories
   # => Returns current user's memories
   def index
     @memories = current_devise_api_user.memories
-
     render json: @memories, status: :ok
   end
 
-  # GET /memories/1 or /memories/1.json
+  # GET /api/memories/1 or /memories/1.json
   def show
     render json: @memory, status: :ok
   end
 
-  # POST /memories
+  # POST /api/memories
   def create
     if params[:memory][:upload_images].present?
       params[:memory][:images] = params[:memory][:upload_images]
     end
 
     update_params = memory_params.except(:remove_images, :upload_images)
-    @memory = current_user.memories.new(update_params)
+    @memory = current_devise_api_user.memories.new(update_params)
 
-    respond_to do |format|
-      if @memory.save
-        format.json { render json: @memory, status: :created }
-      else
-        format.json { render json: @memory.errors, status: :unprocessable_entity }
-      end
+    if @memory.save
+      render json: @memory, status: :created
+    else
+      render json: @memory.errors, status: :unprocessable_entity 
     end
   end
 
-  # PUT /memories/:id
+  # PUT /api/memories/:id
   def update
     # Remove images
     if params[:memory][:remove_images].present?
@@ -51,27 +48,20 @@ class Api::MemoriesController < ApplicationController
 
     update_params = memory_params.except(:remove_images, :upload_images, :images)
 
-    respond_to do |format|
-      if @memory.update(update_params)
-        format.json { render json: @memory, status: :ok }
-      else
-        format.json { render json: @memory.errors, status: :unprocessable_entity }
-      end
+    if @memory.update(update_params)
+      render json: @memory, status: :ok 
+    else
+      render json: @memory.errors, status: :unprocessable_entity 
     end
   end
 
-  # DELETE /memories/:id
+  # DELETE /api/memories/:id
   def destroy
-    if (@memory.user != current_user)
-      respond_to do |format|
-        format.json { render json: { message: "Unprocessable entity." }, status: :unprocessable_entity }
-      end
+    if (@memory.user != current_devise_api_user)
+      render json: { message: "Unprocessable entity." }, status: :unprocessable_entity 
     else
       @memory.destroy!
-
-      respond_to do |format|
-        format.json { head :no_content }
-      end
+      head :no_content 
     end
   end
 
